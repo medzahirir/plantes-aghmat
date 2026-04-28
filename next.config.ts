dc69548afebbca13fcd70598011465fa0ab57499
medzahirir <66330139+medzahirir@users.noreply.github.com>
2026-04-28 23:51:28 +0000

    Update headers to include Content-Security-Policy

--- next.config.ts
+++ next.config.ts
@@ -76,7 +76,12 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/(.*)",
-        headers: securityHeaders,
+         headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval';",
+          },
+        ],
       },
     ];
   },
